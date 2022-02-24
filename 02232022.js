0403af9f3745a0c53e23bc3c56d366adc477a7c1
Mendel Gordon <mendelgordons@gmail.com>
2022-02-23 20:59:18 -0500

    Refactor monthly click for edge case

--- 02232022.js
+++ 02232022.js
@@ -1,32 +1,39 @@
-const bronze = document.querySelector("#bronze");
-const silver = document.querySelector("#silver");
-const gold = document.querySelector("#gold");
-const input = document.querySelector(".fs-donate-amounts.js-donate-amounts > input");
-const donate = document.querySelector(".fs-donate.fs-donatable.text-center > div.button-wrapper.col-md-6 > a");
+const bronze = document.querySelector('#bronze');
+const silver = document.querySelector('#silver');
+const gold = document.querySelector('#gold');
+const input = document.querySelector(
+  '.fs-donate-amounts.js-donate-amounts > input'
+);
+const donate = document.querySelector(
+  '.fs-donate.fs-donatable > .button-wrapper > a'
+);
 const clickDonate = () => {
   donate.click();
-}
+};
 const clickMonthly = () => {
-  // press monthly after a delay of 0.25 seconds
-  setTimeout(function() {
-    document.querySelector("#inputRecurring").click();
-  }, 250);
-}
+  const monthly = document.querySelector('#inputRecurring');
+  // if monthly was not yet pressed, press monthly after a delay of 0.25 seconds
+  if (!monthly.checked) {
+    setTimeout(function () {
+      monthly.click();
+    }, 250);
+  }
+};
 // onclick of bronze type type 108 into the input field, press donate button, and press monthly
-bronze.onclick = function() {
+bronze.onclick = function () {
   input.value = 108;
   clickDonate();
   clickMonthly();
-}
+};
 // onclick of silver type type 180 into the input field, press donate button, and press monthly
-silver.onclick = function() {
+silver.onclick = function () {
   input.value = 180;
   clickDonate();
   clickMonthly();
-}
+};
 // onclick of gold type type 250 into the input field, press donate button, and press monthly
-gold.onclick = function() {
+gold.onclick = function () {
   input.value = 250;
   clickDonate();
   clickMonthly();
-}
+};

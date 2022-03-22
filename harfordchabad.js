5af5284a7c91cd717eae9348ca9ecf12ff1971c2
Mendel Gordon <mendelgordons@gmail.com>
2022-03-22 19:33:26 -0400

    Refector

--- harfordchabad.js
+++ harfordchabad.js
@@ -1,11 +1,13 @@
 if (Co.ArticleId === '5468598') {
-   const donateButton = document.querySelector('.js-action-open-donate');
+   // select all divs on the page
+   const divs = document.querySelectorAll('div');
    const inputRecurring = document.querySelector('#inputRecurring');
    // when the donate button is clicked, click the recurring donation button after a delay
-   donateButton.addEventListener('click', () => {
+   divs.addEventListener('click', () => {
       setTimeout(() => {
          inputRecurring.click();
          inputRecurring.checked = true;
+         console.log('clicked');
       }, 250);
    });
 }

85207126b098b6dfed8fd2a4063ae7d465af831c
Mendel Gordon <mendelgordons@gmail.com>
2022-03-22 19:35:47 -0400

    Refactor

--- harfordchabad.js
+++ harfordchabad.js
@@ -2,12 +2,18 @@ if (Co.ArticleId === '5468598') {
    // select all divs on the page
    const divs = document.querySelectorAll('div');
    const inputRecurring = document.querySelector('#inputRecurring');
-   // when the donate button is clicked, click the recurring donation button after a delay
-   divs.addEventListener('click', () => {
-      setTimeout(() => {
-         inputRecurring.click();
-         inputRecurring.checked = true;
-         console.log('clicked');
-      }, 250);
-   });
+   // when any of the divs are clicked, click the recurring donation button after a delay
+   divs.forEach(div =>
+      div.addEventListener('click', () => {
+         setTimeout(() => {
+            if (inputRecurring) {
+               inputRecurring.click();
+               inputRecurring.checked = true;
+               console.log('Recurring donation clicked');
+            } else {
+               console.log('Recurring donation not found');
+            }
+         }, 250);
+      })
+   );
 }

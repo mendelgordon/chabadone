24d77c1ea8e0eafc4d2192b60bdc354d631c1817
Mendel Gordon <mendelgordons@gmail.com>
2022-03-22 18:11:01 -0400

    Refactor

--- harfordchabad.js
+++ harfordchabad.js
@@ -2,7 +2,7 @@ let codeRun = false;
 if (Co.ArticleId === '5468598' && !codeRun) {
    // when #inputRecurring is added to the dom, select it, unless it's already selected
    const inputRecurring = document.querySelector('#inputRecurring');
-   const select = item => (item.selected ? item : item.click());
+   const select = item => (item.selected ? (item.selected = true) : item.click());
    // check if inputRecurring is in the dom every second
    setInterval(() => inputRecurring && select(inputRecurring), 1000);
    codeRun = true;

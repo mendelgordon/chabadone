94b4c0188d45cb2ae4a804fd87472f9a066d21a7
Mendel Gordon <mendelgordons@gmail.com>
2022-03-22 20:06:58 -0400

    Refactor

--- harfordchabad.js
+++ harfordchabad.js
@@ -4,20 +4,23 @@ if (Co.ArticleId === '5468598') {
    const buttonThree = document.querySelector('div.button-wrapper.col-md-6 > a');
    const buttonFour = document.querySelector('#donors > div > div.js-donors-list > div > a');
    // array of buttons
-   const buttons = [buttonOne, buttonTwo, buttonThree, buttonFour];
-   const inputRecurring = document.querySelector('#inputRecurring');
-   const inputRecurringLabel = document.querySelector('.fs-donate-section > div:nth-child(4) > label');
+   let inputRecurring = document.querySelector('#inputRecurring');
+   let inputRecurringLabel = document.querySelector('.fs-donate-section > div:nth-child(4) > label');
    const clickRecurring = () => {
       setTimeout(() => {
-         if (inputRecurringLabel && inputRecurring.checked === false) {
+         if (inputRecurring.checked === false) {
             inputRecurringLabel.click();
-            inputRecurring.checked = true;
             console.log('Recurring donation clicked');
          } else {
-            console.log('Recurring donation not found');
+            inputRecurring.checked = true;
+            console.log('Recurring donation checked');
          }
       }, 250);
+      return true;
    };
    // when any of the buttons are clicked, click the recurring button
-   buttons.forEach(button => button.addEventListener('click', clickRecurring));
+   buttonOne.addEventListener('click', clickRecurring);
+   buttonTwo.addEventListener('click', clickRecurring);
+   buttonThree.addEventListener('click', clickRecurring);
+   buttonFour.addEventListener('click', clickRecurring);
 }

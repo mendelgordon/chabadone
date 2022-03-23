dffe5f17bf92441b14cfba1e7d7ff46984319be8
Mendel Gordon <mendelgordons@gmail.com>
2022-03-22 20:32:36 -0400

    Refactor

--- harfordchabad.js
+++ harfordchabad.js
@@ -1,9 +1,17 @@
-if (Co.ArticleId === '5468598') {
+const pageURL = window.location.href === 'https://www.harfordchabad.org/templates/fundraising/default_cdo/aid/5468598/jewish/Campaign.htm';
+console.log(pageURL);
+if (pageURL) {
+   console.log('harfordchabad.js');
    const buttonOne = document.querySelector('div.fs-top-tools > div > a');
+   console.log(buttonOne);
    const buttonTwo = document.querySelector('div.jumbotron.fs-header > div > div > div > div:nth-child(1) > div');
+   console.log(buttonTwo);
    const buttonThree = document.querySelector('div.button-wrapper.col-md-6 > a');
+   console.log(buttonThree);
    let inputRecurring = document.querySelector('#inputRecurring');
+   console.log(inputRecurring);
    let inputRecurringLabel = document.querySelector('.fs-donate-section > div:nth-child(4) > label');
+   console.log(inputRecurringLabel);
    const clickRecurring = () => {
       console.log('clicked');
       setTimeout(() => {
@@ -17,8 +25,12 @@ if (Co.ArticleId === '5468598') {
       }, 250);
       return true;
    };
+   console.log(clickRecurring);
    // when any of the buttons are clicked, click the recurring button
    buttonOne.addEventListener('click', clickRecurring);
+   console.log('buttonOne event listener added');
    buttonTwo.addEventListener('click', clickRecurring);
+   console.log('buttonTwo event listener added');
    buttonThree.addEventListener('click', clickRecurring);
+   console.log('buttonThree event listener added');
 }

169de1621147b1fbe65baecd0129b21ba0e18c75
Mendel Gordon <mendelgordons@gmail.com>
2022-04-01 16:13:08 -0400

    Refactor

--- chabadfire/scripts.js
+++ chabadfire/scripts.js
@@ -1,30 +1,85 @@
 window.onload = () => {
-   const slideshow = `<div id="slideshow-example" data-component="slideshow"><div role="list"><div class="slide"><img src="https://chabad.netlify.app/chabadfire/%20(1).jpeg" alt=""></div><div class="slide"><img src="https://chabad.netlify.app/chabadfire/%20(2).jpeg" alt=""></div><div class="slide"><img src="https://chabad.netlify.app/chabadfire/%20(3).jpeg" alt=""></div><div class="slide"><img src="https://chabad.netlify.app/chabadfire/%20(4).jpeg" alt=""></div><div class="slide"><img src="https://chabad.netlify.app/chabadfire/%20(5).jpeg" alt=""></div><div class="slide"><img src="https://chabad.netlify.app/chabadfire/%20(6).jpeg" alt=""></div></div></div><style>[data-component="slideshow"] .slide {display: none;text-align:center} [data-component="slideshow"] .slide.active {display: block}.slide img {max-width:400px;border-radius: 10%;width:100%}</style>`;
+   const slideshow = `<div class="slider"><div class="slide active"><img src="https://chabad.netlify.app/chabadfire/%20(1).jpeg" alt=""></div><div class="slide"><img src="https://chabad.netlify.app/chabadfire/%20(2).jpeg" alt=""></div><div class="slide"><img src="https://chabad.netlify.app/chabadfire/%20(3).jpeg" alt=""></div><div class="slide"><img src="https://chabad.netlify.app/chabadfire/%20(4).jpeg" alt=""></div><div class="slide"><img src="https://chabad.netlify.app/chabadfire/%20(5).jpeg" alt=""></div><div class="slide"><img src="https://chabad.netlify.app/chabadfire/%20(6).jpeg" alt=""></div><div class="navigation"><i class="fas fa-chevron-left prev-btn"></i><i class="fas fa-chevron-right next-btn"></i></div><div class="navigation-visibility"><div class="slide-icon active"></div><div class="slide-icon"></div><div class="slide-icon"></div><div class="slide-icon"></div><div class="slide-icon"></div><div class="slide-icon"></div></div></div>`;
    const pageHeader = document.querySelector('.page-header');
    pageHeader.insertAdjacentHTML('afterend', slideshow);
 
-   var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+   const slider = document.querySelector('.slider');
+   const nextBtn = document.querySelector('.next-btn');
+   const prevBtn = document.querySelector('.prev-btn');
+   const slides = document.querySelectorAll('.slide');
+   const slideIcons = document.querySelectorAll('.slide-icon');
+   const numberOfSlides = slides.length;
+   var slideNumber = 0;
 
-   // Apply to all slideshows that you define with the markup wrote
-   slideshows.forEach(initSlideShow);
+   //image slider next button
+   nextBtn.addEventListener('click', () => {
+      slides.forEach(slide => {
+         slide.classList.remove('active');
+      });
+      slideIcons.forEach(slideIcon => {
+         slideIcon.classList.remove('active');
+      });
 
-   function initSlideShow(slideshow) {
-      var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); // Get an array of slides
+      slideNumber++;
 
-      var index = 0,
-         time = 5000;
-      slides[index].classList.add('active');
+      if (slideNumber > numberOfSlides - 1) {
+         slideNumber = 0;
+      }
 
-      setInterval(() => {
-         slides[index].classList.remove('active');
+      slides[slideNumber].classList.add('active');
+      slideIcons[slideNumber].classList.add('active');
+   });
 
-         //Go over each slide incrementing the index
-         index++;
+   //image slider previous button
+   prevBtn.addEventListener('click', () => {
+      slides.forEach(slide => {
+         slide.classList.remove('active');
+      });
+      slideIcons.forEach(slideIcon => {
+         slideIcon.classList.remove('active');
+      });
 
-         // If you go over all slides, restart the index to show the first slide and start again
-         if (index === slides.length) index = 0;
+      slideNumber--;
 
-         slides[index].classList.add('active');
-      }, time);
-   }
+      if (slideNumber < 0) {
+         slideNumber = numberOfSlides - 1;
+      }
+
+      slides[slideNumber].classList.add('active');
+      slideIcons[slideNumber].classList.add('active');
+   });
+
+   //image slider autoplay
+   var playSlider;
+
+   var repeater = () => {
+      playSlider = setInterval(function () {
+         slides.forEach(slide => {
+            slide.classList.remove('active');
+         });
+         slideIcons.forEach(slideIcon => {
+            slideIcon.classList.remove('active');
+         });
+
+         slideNumber++;
+
+         if (slideNumber > numberOfSlides - 1) {
+            slideNumber = 0;
+         }
+
+         slides[slideNumber].classList.add('active');
+         slideIcons[slideNumber].classList.add('active');
+      }, 4000);
+   };
+   repeater();
+
+   //stop the image slider autoplay on mouseover
+   slider.addEventListener('mouseover', () => {
+      clearInterval(playSlider);
+   });
+
+   //start the image slider autoplay again on mouseout
+   slider.addEventListener('mouseout', () => {
+      repeater();
+   });
 };

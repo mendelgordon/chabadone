475520f16863c8557d7bb724b6d5ac1ee95b02ea
Mendel Gordon <mendelgordons@gmail.com>
2022-05-02 23:38:07 -0400

    Add logo, resize

--- chabadsola.js
+++ chabadsola.js
@@ -2,7 +2,7 @@
 window.addEventListener('DOMContentLoaded', () => {
    try {
       const donateDescription = document.querySelector('#js-donate-form > div.content-box.full-row.intro-wrapper > div > p');
-      const newDonateDescription = `<a href="/5430417">To donate with Venmo click here</a><br /><a href="https://www.theeidenproject.org">To donate toward construction of the new building click here</a><br /><a href="/3438459">To reserve for Friday night dinner click here</a>`;
+      const newDonateDescription = `<a href="/5430417">To donate with Venmo click here <img src="https://cdn1.venmo.com/marketing/images/branding/venmo-icon.svg" style="height: 14px;"></a><br /><a href="https://www.theeidenproject.org">To donate toward construction of the new building click here</a><br /><a href="/3438459">To reserve for Friday night dinner click here</a>`;
       donateDescription.innerHTML = newDonateDescription;
    } catch (error) {
       console.log(error);

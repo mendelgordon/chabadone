35025926aa2405fb43431be58b4f52b1ebd63f1b
Mendel Gordon <mendelgordons@gmail.com>
2022-05-05 17:10:16 -0400

    Upload js

---
+++ chabadwaterways.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', () => {
+   if (Co.ArticleId == '5512769') {
+      let items = document.querySelectorAll('#cid_3 label span');
+      items.forEach(element => {
+         if (element.innerHTML.includes('-')) {
+            element.innerHTML.split('-');
+            element.innerHTML = '<strong>' + element.innerHTML.replace('-', '</strong> - ');
+         }
+         let spanText = element.innerText.toLowerCase();
+         if (spanText.includes('reserved') || spanText.includes('dedicated')) {
+            element.closest('.form-checkbox-item').classList.add('reserved-dedication');
+         }
+      });
+      document.querySelector('#Head1').insertAdjacentHTML('beforeend', '<link rel="stylesheet" href="https://chabad.netlify.app/chabadwaterways.css" />');
+   }
+});

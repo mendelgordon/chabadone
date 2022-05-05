998d088326b90cd73d7faf27e127b364783b8407
Mendel Gordon <mendelgordons@gmail.com>
2022-05-05 11:22:53 -0400

    Make recurrence default and hidden

--- chabadsola.js
+++ chabadsola.js
@@ -1,5 +1,6 @@
 /* when the document loads, run the function */
 window.addEventListener('DOMContentLoaded', () => {
+   /* new donate page */
    try {
       const donateDescription = document.querySelector('#js-donate-form > div.content-box.full-row.intro-wrapper > div > p');
       const newDonateDescription = `<a href="/thesolatent">For The SOLA Tent click here</a><br /><a href="/5430417">To donate with Venmo click here <img src="https://cdn1.venmo.com/marketing/images/branding/venmo-icon.svg" style="height: 14px;"></a><br /><a href="https://www.theeidenproject.org">To donate to the Eiden Project click here</a><br /><a href="/3438459">To reserve for Friday night dinner click here</a>`;
@@ -7,6 +8,14 @@ window.addEventListener('DOMContentLoaded', () => {
       document.querySelector('label[for=donation_subtype]').innerHTML = 'Apply Donation To...<span class="req">*</span>';
       document.querySelector('select[name=donation_subtype]').required = 1;
       document.querySelector('#subscribe').checked = 0;
+   } catch (error) {
+      console.log(error);
+   }
+   /* SOLA tent membership page */
+   try {
+      const tentMembership = document.querySelector('input[value=Monthly]');
+      tentMembership.checked = 1;
+      tentMembership.closest('.form-line').style.display = 'none';
    } catch (error) {
       console.log(error);
    }

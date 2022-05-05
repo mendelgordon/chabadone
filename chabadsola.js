97b8881fc138bc2d8aa3ce895555c97001ebfa47
Mendel Gordon <mendelgordons@gmail.com>
2022-05-05 18:26:00 -0400

    Uppercase crypto

--- chabadsola.js
+++ chabadsola.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', () => {
    /* new donate page */
    try {
       const donateDescription = document.querySelector('#js-donate-form > div.content-box.full-row.intro-wrapper > div > p');
-      const newDonateDescription = `<a href="/thesolatent">For The SOLA Tent click here</a><br /><a href="/crypto">To donate in crypto click here <img src="https://thegivingblock.com/wp-content/uploads/2021/07/Bitcoin-btc-Logo.png" style="height:14px"></a><br /><a href="/5430417">To donate with Venmo click here <img src="https://cdn1.venmo.com/marketing/images/branding/venmo-icon.svg" style="height: 14px;"></a><br /><a href="https://www.theeidenproject.org">To donate to the Eiden Project click here</a><br /><a href="/3438459">To reserve for Friday night dinner click here</a>`;
+      const newDonateDescription = `<a href="/thesolatent">For The SOLA Tent click here</a><br /><a href="/crypto">To donate in Crypto click here <img src="https://thegivingblock.com/wp-content/uploads/2021/07/Bitcoin-btc-Logo.png" style="height:14px"></a><br /><a href="/5430417">To donate with Venmo click here <img src="https://cdn1.venmo.com/marketing/images/branding/venmo-icon.svg" style="height: 14px;"></a><br /><a href="https://www.theeidenproject.org">To donate to the Eiden Project click here</a><br /><a href="/3438459">To reserve for Friday night dinner click here</a>`;
       donateDescription.innerHTML = newDonateDescription;
       document.querySelector('label[for=donation_subtype]').innerHTML = 'Apply Donation To...<span class="req">*</span>';
       document.querySelector('select[name=donation_subtype]').required = 1;

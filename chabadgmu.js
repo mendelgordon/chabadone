7797554390ea4454237b85e833d0c8b1c06d6d2d
Mendel Gordon <mendelgordons@gmail.com>
2022-06-10 10:43:17 -0400

    Upload

---
+++ chabadgmu.js
@@ -0,0 +1,8 @@
+/* when the document loads, run the function */
+window.addEventListener("DOMContentLoaded", () => {
+  /* new donate page */
+  if (window.location.href.includes("4970020")) {
+    document.querySelector("div.footnotes div.disclaimer").innerHTML = 'Have any questions or need help making your donation? Please call us at <span class="phone-number">414-807-4533</span>\n\t\t\t\t\t\t\t\t  or email&nbsp;<a href="mailto:rabbi@chabadgmu.com">rabbi@chabadgmu.com</a>\n\t\t\t\t\t\t\t\t  \n\t\t\t\t\t\t\t  ';
+    document.querySelector(".description").insertAdjacentHTML("beforeend", "<b><br>Zelle: rabbi@chabadgmu.com<br><a href='www.paypal.com/paypalme/chabadgmu'>Click here for PayPal</a><br><a href='https://cash.app/$chabadgmu'>Click here for Cashapp</a></b>");
+  }
+});

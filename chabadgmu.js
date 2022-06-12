0c7a1f92875d797d3907f0cfa03808cc787cee75
Mendel Gordon <mendelgordons@gmail.com>
2022-06-12 17:47:44 -0400

    Update phone

--- chabadgmu.js
+++ chabadgmu.js
@@ -5,4 +5,8 @@ window.addEventListener("DOMContentLoaded", () => {
     document.querySelector("div.footnotes div.disclaimer").innerHTML = 'Have any questions or need help making your donation? Please call us at <span class="phone-number">414-807-4533</span>\n\t\t\t\t\t\t\t\t  or email&nbsp;<a href="mailto:rabbi@chabadgmu.com">rabbi@chabadgmu.com</a>\n\t\t\t\t\t\t\t\t  \n\t\t\t\t\t\t\t  ';
     document.querySelector(".description").insertAdjacentHTML("beforeend", document.querySelector(".donation-options-other").innerHTML);
   }
+
+  if (window.location.href.includes("5549418")) {
+    document.querySelector(".fs-footer address").innerHTML = "'\n<h3>Chabad of George Mason University</h3>\n4858 Sideburn Road Fairfax, VA 22032\n<br>\n414-807-4533\n'";
+  }
 });

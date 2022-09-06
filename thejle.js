7a67c0927c00a02dc84de76fb60aa6f6e65a4590
Mendel Gordon <mendelgordons@gmail.com>
2022-09-06 14:48:13 -0400

    Upload jle js

---
+++ thejle.js
@@ -0,0 +1,7 @@
+const popupSubscribeMessage = document.querySelector("#PopupSubscribeMessage");
+if (popupSubscribeMessage) {
+	const firstLastNameInputs = '<div class="clearfix" style="padding: 4px 0;"><input type="text" maxlength="50" class="co_global_input float_left active" style="width: 49%;" value="" required="" display_name="First Name" id="Fname" name="fname" placeholder="First Name"><input type="text" maxlength="50" class="co_global_input float_right active" style="width: 49%;" value="" required="" display_name="Last Name" id="Lname" name="lname" placeholder="Last Name"></div>';
+	const popupSubscribeForm = document.querySelector("#PopupSubscribeMessage form div div");
+	popupSubscribeForm.style.height = "auto";
+	popupSubscribeForm.insertAdjacentHTML("afterbegin", firstLastNameInputs);
+}

5686a9303d43aad1eedbf27da8d449e0f4b591be
Mendel Gordon <mendelgordons@gmail.com>
2022-09-20 12:46:05 -0400

    Add keyframes

--- chabadbinghamfarms.js
+++ chabadbinghamfarms.js
@@ -1,9 +1,42 @@
 if (window.location.href.includes(5637523)) {
- let items = document.querySelectorAll("select option");
- items.forEach((element) => {
-  if (element.innerHTML.toLowerCase().includes("reserved")) {
-   element.disabled = true;
-   console.log(`Disabled ${element.innerHTML} because it is reserved`);
-  }
- });
+	let items = document.querySelectorAll("select option");
+	items.forEach((element) => {
+		if (element.innerHTML.toLowerCase().includes("reserved")) {
+			element.disabled = true;
+			console.log(`Disabled ${element.innerHTML} because it is reserved`);
+		}
+	});
+	const entries = document.querySelectorAll(".form-line");
+	const callback = (entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add("fadeIn");
+				// stop observing this element
+				observer.unobserve(entry.target);
+			}
+		});
+	};
+	const options = {};
+	const myObserver = new IntersectionObserver(callback, options);
+	entries.forEach((formLine) => {
+		myObserver.observe(formLine);
+	});
+}
+
+if (window.location.href.includes(5637447)) {
+	const entries = document.querySelectorAll(".co_body img");
+	const callback = (entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add("fadeIn");
+				// stop observing this element
+				observer.unobserve(entry.target);
+			}
+		});
+	};
+	const options = {};
+	const myObserver = new IntersectionObserver(callback, options);
+	entries.forEach((img) => {
+		myObserver.observe(img);
+	});
 }

b2d22b871c517f29561bd222a9e89d921e817c44
Mendel Gordon <mendelgordons@gmail.com>
2022-09-21 10:52:57 -0400

    delay longer

--- chabadfire/scripts.js
+++ chabadfire/scripts.js
@@ -1,61 +1,19 @@
 const slideshow = `<div class="slider"><div class="slide active"><img src="https://w2.chabad.org/media/images/1203/aDYT12036898.jpeg"</div><div class="slide"><img src="https://w2.chabad.org/media/images/1203/kKko12036900.jpeg"</div><div class="slide"><img src="https://w2.chabad.org/media/images/1203/IkWs12036902.jpeg"</div><div class="slide"><img src="https://w2.chabad.org/media/images/1203/yYwj12036904.jpeg"</div><div class="slide"><img src="https://w2.chabad.org/media/images/1203/yYwj12036904.jpeg"</div><div class="slide"><img src="https://w2.chabad.org/media/images/1203/LyBw12036908.jpeg"</div><div class="navigation"><i class="fas fa-chevron-left prev-btn"></i><i class="fas fa-chevron-right next-btn"></i></div><div class="navigation-visibility"><div class="slide-icon active"></div><div class="slide-icon"></div><div class="slide-icon"></div><div class="slide-icon"></div><div class="slide-icon"></div><div class="slide-icon"></div></div></div>`;
 const pageHeader = document.querySelector(".page-header");
-/* after a one second delay, the slideshow will be added to the page header */
-setTimeout(() => {
+/* after a five second delay run the remaining code */
+setTimeout(function () {
 	pageHeader.insertAdjacentHTML("afterend", slideshow);
-}, 1000);
 
-const slider = document.querySelector(".slider");
-const nextBtn = document.querySelector(".next-btn");
-const prevBtn = document.querySelector(".prev-btn");
-const slides = document.querySelectorAll(".slide");
-const slideIcons = document.querySelectorAll(".slide-icon");
-const numberOfSlides = slides.length;
-var slideNumber = 0;
+	const slider = document.querySelector(".slider");
+	const nextBtn = document.querySelector(".next-btn");
+	const prevBtn = document.querySelector(".prev-btn");
+	const slides = document.querySelectorAll(".slide");
+	const slideIcons = document.querySelectorAll(".slide-icon");
+	const numberOfSlides = slides.length;
+	var slideNumber = 0;
 
-//image slider next button
-nextBtn.addEventListener("click", () => {
-	slides.forEach((slide) => {
-		slide.classList.remove("active");
-	});
-	slideIcons.forEach((slideIcon) => {
-		slideIcon.classList.remove("active");
-	});
-
-	slideNumber++;
-
-	if (slideNumber > numberOfSlides - 1) {
-		slideNumber = 0;
-	}
-
-	slides[slideNumber].classList.add("active");
-	slideIcons[slideNumber].classList.add("active");
-});
-
-//image slider previous button
-prevBtn.addEventListener("click", () => {
-	slides.forEach((slide) => {
-		slide.classList.remove("active");
-	});
-	slideIcons.forEach((slideIcon) => {
-		slideIcon.classList.remove("active");
-	});
-
-	slideNumber--;
-
-	if (slideNumber < 0) {
-		slideNumber = numberOfSlides - 1;
-	}
-
-	slides[slideNumber].classList.add("active");
-	slideIcons[slideNumber].classList.add("active");
-});
-
-//image slider autoplay
-var playSlider;
-
-var repeater = () => {
-	playSlider = setInterval(function () {
+	//image slider next button
+	nextBtn.addEventListener("click", () => {
 		slides.forEach((slide) => {
 			slide.classList.remove("active");
 		});
@@ -71,16 +29,58 @@ var repeater = () => {
 
 		slides[slideNumber].classList.add("active");
 		slideIcons[slideNumber].classList.add("active");
-	}, 4000);
-};
-repeater();
+	});
 
-//stop the image slider autoplay on mouseover
-slider.addEventListener("mouseover", () => {
-	clearInterval(playSlider);
-});
+	//image slider previous button
+	prevBtn.addEventListener("click", () => {
+		slides.forEach((slide) => {
+			slide.classList.remove("active");
+		});
+		slideIcons.forEach((slideIcon) => {
+			slideIcon.classList.remove("active");
+		});
 
-//start the image slider autoplay again on mouseout
-slider.addEventListener("mouseout", () => {
+		slideNumber--;
+
+		if (slideNumber < 0) {
+			slideNumber = numberOfSlides - 1;
+		}
+
+		slides[slideNumber].classList.add("active");
+		slideIcons[slideNumber].classList.add("active");
+	});
+
+	//image slider autoplay
+	var playSlider;
+
+	var repeater = () => {
+		playSlider = setInterval(function () {
+			slides.forEach((slide) => {
+				slide.classList.remove("active");
+			});
+			slideIcons.forEach((slideIcon) => {
+				slideIcon.classList.remove("active");
+			});
+
+			slideNumber++;
+
+			if (slideNumber > numberOfSlides - 1) {
+				slideNumber = 0;
+			}
+
+			slides[slideNumber].classList.add("active");
+			slideIcons[slideNumber].classList.add("active");
+		}, 4000);
+	};
 	repeater();
-});
+
+	//stop the image slider autoplay on mouseover
+	slider.addEventListener("mouseover", () => {
+		clearInterval(playSlider);
+	});
+
+	//start the image slider autoplay again on mouseout
+	slider.addEventListener("mouseout", () => {
+		repeater();
+	});
+}, 5000);

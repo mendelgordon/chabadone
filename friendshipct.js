c7949ff1fb868895b93872e0ccf0cb6891b0520d
Mendel Gordon <mendelgordons@gmail.com>
2022-11-24 22:17:52 -0500

    Hide part 2 of form for FC

--- friendshipct.js
+++ friendshipct.js
@@ -1,5 +1,6 @@
 const CURRENTPATHNAME = window.location.pathname;
 const GATHERINGOFUNITY = CURRENTPATHNAME.includes('5689686') || CURRENTPATHNAME.includes('5689710') || CURRENTPATHNAME.includes('5689724') || CURRENTPATHNAME.includes('5689778') || CURRENTPATHNAME.includes('5692000');
+const VOLUNTEERFORM = CURRENTPATHNAME.includes('5151994');
 
 if (GATHERINGOFUNITY) {
  // ADD LINK TO STYLE TO HEAD
@@ -7,4 +8,25 @@ if (GATHERINGOFUNITY) {
  LINK.rel = 'stylesheet';
  LINK.href = 'https://chabad.netlify.app/friendshipct.css';
  document.head.appendChild(LINK);
+}
+
+if (VOLUNTEERFORM) {
+ window.addEventListener('DOMContentLoaded', () => {
+  const partTwoInput = document.querySelector('#input_60_0');
+  const partTwo = document.querySelectorAll('#id_60 ~ *');
+  const stayHidden = document.querySelector('input[name="website"]').parentElement;
+
+  partTwo.forEach((el) => {
+   el.style.display = 'none';
+  });
+
+  partTwoInput.addEventListener('change', () => {
+   if (partTwoInput.checked) {
+    partTwo.forEach((el) => {
+     el.style.display = 'grid';
+    });
+    stayHidden.style.display = 'none';
+   }
+  });
+ });
 }

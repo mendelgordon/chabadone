7b0c56961079b4f03caab7312d702b8ae96c2771
Mendel Gordon <mendelgordons@gmail.com>
2022-11-24 22:23:49 -0500

    Move control to separate function to clean code

--- friendshipct.js
+++ friendshipct.js
@@ -1,32 +1,36 @@
 const CURRENTPATHNAME = window.location.pathname;
-const GATHERINGOFUNITY = CURRENTPATHNAME.includes('5689686') || CURRENTPATHNAME.includes('5689710') || CURRENTPATHNAME.includes('5689724') || CURRENTPATHNAME.includes('5689778') || CURRENTPATHNAME.includes('5692000');
-const VOLUNTEERFORM = CURRENTPATHNAME.includes('5151994');
+const GATHERINGOFUNITY = CURRENTPATHNAME.includes("5689686") || CURRENTPATHNAME.includes("5689710") || CURRENTPATHNAME.includes("5689724") || CURRENTPATHNAME.includes("5689778") || CURRENTPATHNAME.includes("5692000");
+const VOLUNTEERFORM = CURRENTPATHNAME.includes("5151994");
 
 if (GATHERINGOFUNITY) {
- // ADD LINK TO STYLE TO HEAD
- const LINK = document.createElement('link');
- LINK.rel = 'stylesheet';
- LINK.href = 'https://chabad.netlify.app/friendshipct.css';
- document.head.appendChild(LINK);
+	// ADD LINK TO STYLE TO HEAD
+	const LINK = document.createElement("link");
+	LINK.rel = "stylesheet";
+	LINK.href = "https://chabad.netlify.app/friendshipct.css";
+	document.head.appendChild(LINK);
 }
 
 if (VOLUNTEERFORM) {
- window.addEventListener('DOMContentLoaded', () => {
-  const partTwoInput = document.querySelector('#input_60_0');
-  const partTwo = document.querySelectorAll('#id_60 ~ *');
-  const stayHidden = document.querySelector('input[name="website"]').parentElement;
+	window.addEventListener("DOMContentLoaded", () => {
+		const partTwoInput = document.querySelector("#input_60_0");
+		const partTwo = document.querySelectorAll("#id_60 ~ *");
+		const stayHidden = document.querySelector('input[name="website"]').parentElement;
 
-  partTwo.forEach((el) => {
-   el.style.display = 'none';
-  });
+		const controlDisplayOfFormFields = () => {
+			if (partTwoInput.checked) {
+				partTwo.forEach((el) => {
+					el.style.display = "grid";
+				});
+				stayHidden.style.display = "none";
+			} else {
+				partTwo.forEach((el) => {
+					el.style.display = "none";
+				});
+			}
+		};
 
-  partTwoInput.addEventListener('change', () => {
-   if (partTwoInput.checked) {
-    partTwo.forEach((el) => {
-     el.style.display = 'grid';
-    });
-    stayHidden.style.display = 'none';
-   }
-  });
- });
+		controlDisplayOfFormFields();
+
+		partTwoInput.addEventListener("change", controlDisplayOfFormFields());
+	});
 }

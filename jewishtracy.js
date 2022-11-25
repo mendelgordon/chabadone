a8e435e1c65145b5f66d53b57fee55df04bb7992
Mendel Gordon <mendelgordons@gmail.com>
2022-11-25 15:59:19 -0500

    Show slideshow on top of page, not bottom

--- jewishtracy.js
+++ jewishtracy.js
@@ -4,9 +4,9 @@ const DONATE = CURRENTPATHNAME.includes("4970020");
 if (DONATE) {
 	const addSlideshow = () => {
 		const iframe = `<iframe allow="autoplay *; fullscreen *" allowfullscreen="" data-dashlane-frameid="3305" frameborder="0" height="225" scrolling="no" src="https://rtcdn.cincopa.com/iframe.aspx?fid=AwKAv6-9ivM-" style="width:100%" width="600"></iframe>`;
-		const wrapper = document.getElementById("donate-wrapper");
+		const wrapper = document.getElementById("ContentBody");
 		// insert iframe after wrapper
-		wrapper.insertAdjacentHTML("afterend", iframe);
+		wrapper.insertAdjacentHTML("afterbegin", iframe);
 	};
 	window.addEventListener("DOMContentLoaded", addSlideshow);
 }

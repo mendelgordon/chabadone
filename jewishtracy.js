165bfaec953a28d254e0326e278cf003df43500d
Mendel Gordon <mendelgordons@gmail.com>
2022-11-27 10:36:52 -0500

    Fix slideshow height

--- jewishtracy.js
+++ jewishtracy.js
@@ -3,7 +3,7 @@ const DONATE = CURRENTPATHNAME.includes("4970020");
 
 if (DONATE) {
 	const addSlideshow = () => {
-		const iframe = `<iframe allow="autoplay *; fullscreen *" allowfullscreen="" data-dashlane-frameid="3305" frameborder="0" height="225" scrolling="no" src="https://rtcdn.cincopa.com/iframe.aspx?fid=AwKAv6-9ivM-" style="width:100%" width="600"></iframe>`;
+		const iframe = `<iframe allow="autoplay *; fullscreen *" allowfullscreen="" data-dashlane-frameid="3305" frameborder="0" height="200" scrolling="no" src="https://rtcdn.cincopa.com/iframe.aspx?fid=AwKAv6-9ivM-" style="width:100%" width="600"></iframe>`;
 		const wrapper = document.getElementById("ContentBody");
 		// insert iframe after wrapper
 		wrapper.insertAdjacentHTML("afterbegin", iframe);

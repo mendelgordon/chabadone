2b437ad3a9ba45122ef85764b87c5ed4a6a0aa19
Mendel Gordon <mendelgordons@gmail.com>
2023-02-06 16:32:48 -0500

    Remove code removing cc option from donate

--- novijewishcenter.js
+++ novijewishcenter.js
@@ -1,29 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
-	/* if on donate page remove credit card payment option */
 	if (window.location.href.includes("4970020")) {
-		/* select cc input fields and remove them from the DOM */
-		const ccInput = document.querySelector(".credit-card-wrapper");
-		if (ccInput) {
-			ccInput.remove();
-		}
-
-		const ccIcon = document.querySelector('.icon-wrapper[data-type="credit-card"]');
-		if (ccIcon) {
-			ccIcon.remove();
-		}
-
-		/* select the next button and click it */
-		const nextButton = document.querySelector(".icon-wrapper");
-		if (nextButton) {
-			nextButton.click();
-		}
-
-		/* select paypal donation instructions and move them from the parent div to the grandparent div, so they are before the donate button in the dom */
-		const paypalInstructions = document.querySelector(".paypal-wrapper");
-		if (paypalInstructions) {
-			/* change innerHTML of paypal	instructions from '\n\t\t\t\t\t\t\t  Click Donate below to complete your donation on PayPal’s site....\n\t\t\t\t\t\t  ' to Click the donate icon below to complete your donation by credit card (We use PayPal to process credit card donations).*/
-			paypalInstructions.innerHTML = "Click the donate icon below to complete your donation by credit card (We use PayPal to process credit card donations).";
-		}
 		/* move dedication/note field to the first column instead of the second */
 		const dedication = document.querySelector("label[for='notes']");
 		if (dedication) {
@@ -31,12 +7,6 @@ window.addEventListener("DOMContentLoaded", () => {
 			/* put container at the end of .content-box.screen-2	*/
 			const contentBox = document.querySelector(".content-box.screen-2");
 			contentBox.appendChild(container);
-		}
-		/* replace the paypal icon with the credit card icon - meaning use the fa-credit-card-alt class instead of fa-cc-paypal class */
-		const paypalIcon = document.querySelector(".fa-cc-paypal");
-		if (paypalIcon) {
-			paypalIcon.classList.remove("fa-cc-paypal");
-			paypalIcon.classList.add("fa-credit-card-alt");
 		}
 	}
 });

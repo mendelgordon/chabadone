d985c36bccfde5fbb189a53f63345586ff33ccfa
Mendel Gordon <mendelgordons@gmail.com>
2023-04-21 10:44:13 -0400

    Add conditional statement to prevent error

--- removeSubscribeDonate.js
+++ removeSubscribeDonate.js
@@ -1,6 +1,7 @@
 /* hide subscribe option on donate page */
 window.addEventListener("load", () => {
   if (window.location.href.includes("4970020")) {
-    document.querySelector('label[for="subscribe"]').closest(".row").style.display = "none";
+    const subscribe = document.querySelector('label[for="subscribe"]');
+    if (subscribe) subscribe.closest(".row").style.display = "none";
   }
 });

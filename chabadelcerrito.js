626fb63fb1aa2b3149bff2b77af9b37ea1825b88
Mendel Gordon <mendelgordons@gmail.com>
2023-07-20 19:36:51 -0400

    Remove potential nulls for easy error catching

--- chabadelcerrito.js
+++ chabadelcerrito.js
@@ -1,9 +1,10 @@
+
 // add a button to the header for the Chai Club
 const donateButtonOne = document.querySelector(".donate-button");
 const headerChaiClub = `<div class="donate-button"><div class="donate-button-text common-donate chai-club">Join the Monthly Giving (Chai) Club!</div></div>`;
 // if headerChaiClub wasn't yet added, add it
 if (!document.querySelector(".chai-club.common-donate")) {
-	donateButtonOne?.insertAdjacentHTML("afterend", headerChaiClub);
+	donateButtonOne.insertAdjacentHTML("afterend", headerChaiClub);
 }
 
 // add a button to the main section for the Chai Club
@@ -11,7 +12,7 @@ const donateSection = document.querySelector(".fs-donate.fs-donatable");
 const mainChaiClub = `<div class="button-wrapper col-md-10"><div class="btn fs-btn chai-club" style="background: #3da7ce;white-space:break-spaces">Join the Monthly Giving (Chai) Club!<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span></div></div>`;
 // if mainChaiClub wasn't yet added, add it
 if (!document.querySelector(".chai-club.fs-btn")) {
-	donateSection?.insertAdjacentHTML("beforeend", mainChaiClub);
+	donateSection.insertAdjacentHTML("beforeend", mainChaiClub);
 }
 
 // make the Chai Club buttons open the donate page with the monthly option selected
@@ -21,10 +22,10 @@ const donateButtonTwo = document.querySelector(".fs-donate.fs-donatable > .butto
 chaiClubButtons.forEach((button) => {
 	button.onclick = function () {
 		// if monthly was not yet pressed, press monthly
-		if (!monthly?.checked) {
-			monthly?.click();
+		if (!monthly.checked) {
+			monthly.click();
 		}
 		// open the donate page
-		donateButtonTwo?.click();
+		donateButtonTwo.click();
 	};
 });

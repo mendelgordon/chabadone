4c751390b506b6ce9cbdb59790cefbcaf40e7214
Mendel Gordon <mendelgordons@gmail.com>
2023-10-28 21:13:34 -0400

    Remove back to site bar

--- chabadofparkland.js
+++ chabadofparkland.js
@@ -1,4 +1,8 @@
 if (window.location.href.includes(6146943)) {
     // add https://chabad.netlify.app/chabadofparkland.css to header
     document.querySelector("#Head1").insertAdjacentHTML("beforeend", '<link rel="stylesheet" href="https://chabad.netlify.app/chabadofparkland.css" />');
+    // once the page is loaded, hide the back to site bar
+    window.addEventListener("DOMContentLoaded", () => {
+        document.querySelector(".cco_templateless_template").remove();
+    })
 }

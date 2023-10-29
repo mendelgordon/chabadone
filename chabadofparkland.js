e47ddfe4177b834e0ff41bf489fed498b2c2019c
Mendel Gordon <mendelgordons@gmail.com>
2023-10-28 20:44:16 -0400

    Create files

---
+++ chabadofparkland.js
@@ -0,0 +1,4 @@
+if (window.location.href.includes(6146943)) {
+    // add https://chabad.netlify.app/chabadofparkland.css to header
+    document.querySelector("#Head1").insertAdjacentHTML("beforeend", '<link rel="stylesheet" href="https://chabad.netlify.app/chabadofparkland.css" />');
+}

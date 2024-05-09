78e7993d96e3709c31fd95784cc1813995f9f70f
Mendel Gordon <mendelgordons@gmail.com>
2024-05-09 09:45:35 -0400

    Add names and sort alphabetically

--- chabadom.js
+++ chabadom.js
@@ -3,6 +3,6 @@ if (window.location.href.includes(5879661)) {
     document.querySelector('.fs-matchers-list').innerHTML = matchers2023;
 }
 if (window.location.href.includes(6430627)) {
-    const matchers2024 = `<div class='row fs-matchers-list'><div class='fs-matcher fs-matcher-right'><h3>Matching Donors</h3><h4 class='fs-matcher-title'>Marat & Leah Kushner<br /><br />Brian Levin & Family<br /><br />Steve & Allison Shapiro<br /><br />In memory of Valery ben Zalman</h4></div><img src='/media/images/1276/EfiK12767809.png' class='heart-donate' /><div class='fs-matcher fs-matcher-left'><h3>Community Pillars</h3><h4 class='fs-matcher-title'>Moshe & Jodie Schwartz<br /><br />Ben & Jesse Terner</h4></div></div>`;
+    const matchers2024 = `<div class='row fs-matchers-list'><div class='fs-matcher fs-matcher-right'><h3>Matching Donors</h3><h4 class='fs-matcher-title'>Marat & Leah Kushner<br /><br />Brian Levin & Family<br /><br />Steve & Allison Shapiro<br /><br />In memory of Valery ben Zalman</h4></div><img src='/media/images/1276/EfiK12767809.png' class='heart-donate' /><div class='fs-matcher fs-matcher-left'><h3>Community Pillars</h3><h4 class='fs-matcher-title'>Ben & Jesse Terner<br /><br />Elliott & Nava Gorbaty<br /><br />Jody Maier<br /><br />Moshe & Jodie Schwartz</h4></div></div>`;
     document.querySelector('.fs-matchers-list').innerHTML = matchers2024;
 }

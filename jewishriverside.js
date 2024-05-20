dc7789eb2275fb1af8a350f7035252a614d16669
Mendel Gordon <mendelgordons@gmail.com>
2024-05-20 11:06:13 -0400

    Update header styling and remove unused code

--- jewishriverside.js
+++ jewishriverside.js
@@ -1,8 +1,10 @@
-window.addEventListener("DOMContentLoaded", () => {
-	if (window.location.href.includes(4970020)) {
-		const subscribe = document.querySelector("label[for='subscribe']");
-		if (subscribe) {
-			subscribe.parentElement.parentElement.parentElement.remove();
-		}
+if (window.location.href.includes(4970020)) {
+	const subscribe = document.querySelector("label[for='subscribe']");
+	if (subscribe) {
+		subscribe.parentElement.parentElement.parentElement.remove();
 	}
-});
+}
+const header = document.querySelector("#header");
+if (header) {
+	header.classList.add("on-scroll");
+}

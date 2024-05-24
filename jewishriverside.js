6e8eaea31d85d3e2f374969b8d337d6cafa5b278
Mendel Gordon <mendelgordons@gmail.com>
2024-05-23 20:46:34 -0400

    Import scroll-timeline polyfill

--- jewishriverside.js
+++ jewishriverside.js
@@ -1,3 +1,5 @@
+import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
+
 if (window.location.href.includes(4970020)) {
 	const subscribe = document.querySelector("label[for='subscribe']");
 	if (subscribe) {

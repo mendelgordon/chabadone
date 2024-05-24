dcadc3d18d5e38b5428bdd1cf1a9d6ef3af4cbcd
Mendel Gordon <mendelgordons@gmail.com>
2024-05-23 20:50:28 -0400

    Switch to script directly on page

--- jewishriverside.js
+++ jewishriverside.js
@@ -1,5 +1,3 @@
-import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
-
 if (window.location.href.includes(4970020)) {
 	const subscribe = document.querySelector("label[for='subscribe']");
 	if (subscribe) {

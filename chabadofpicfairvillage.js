ad60bae1a0d4bd5a850e51ee100bee834ed1a740
Mendel Gordon <mendelgordons@gmail.com>
2024-09-01 04:29:08 -0400

    Account for code being in footer

--- chabadofpicfairvillage.js
+++ chabadofpicfairvillage.js
@@ -1,11 +1,10 @@
 const pageURL = window.location.pathname;
 
 if (pageURL.includes(4970020) && pageURL.includes('highholidays')) {
-    document.addEventListener('DOMContentLoaded', function () {
-        const introWrapper = document.querySelector('.intro-wrapper .col');
+    const introWrapper = document.querySelector('.intro-wrapper .col');
 
-        if (introWrapper) {
-            introWrapper.innerHTML = `
+    if (introWrapper) {
+        introWrapper.innerHTML = `
             <img src="/media/images/1289/CchY12894173.jpeg" style="max-width: min(500px, 100%); margin: auto; display: block;" class="mobile-only" alt="" />
             <h1 style="font-weight: bold;">Support Our Jewish Community during the High Holidays!</h1>
             <p class="description">
@@ -29,27 +28,26 @@ if (pageURL.includes(4970020) && pageURL.includes('highholidays')) {
               Your name(s) will be listed on our Caring Wall, a special display that brings our community together. (Donors who wish to remain anonymous can do so, but still contribute to the sense of community.)
             </p>
           `;
-        }
+    }
 
-        const submitButton = document.querySelector('.js-donate-button');
+    const submitButton = document.querySelector('.js-donate-button');
 
-        if (submitButton) {
-            const newText = document.createElement('p');
-            newText.className = 'submission-additional-text';
-            newText.innerHTML = `
+    if (submitButton) {
+        const newText = document.createElement('p');
+        newText.className = 'submission-additional-text';
+        newText.innerHTML = `
         Every gift counts. Whether you can give $360, $540, or more, your support will have a direct and lasting impact on the people in our community.<br/><br/>
         Together, we can make this year’s High Holidays a time of joy, unity, and spiritual renewal for our community.<br/><br/>
         Thank you for standing with us and helping to keep Jewish traditions alive and vibrant!
       `;
 
-            submitButton.parentElement.appendChild(newText);
-        }
+        submitButton.parentElement.appendChild(newText);
+    }
 
-        const container = document.querySelector('.screen-1');
-        if (container) {
-            container.insertAdjacentHTML('afterbegin', `<aside class="image-bg"></aside>`);
-        }
-    });
+    const container = document.querySelector('.screen-1');
+    if (container) {
+        container.insertAdjacentHTML('afterbegin', `<aside class="image-bg"></aside>`);
+    }
 
     const amountsButtons = document.querySelectorAll(".js-amounts-button");
     const nextButton = document.querySelector("button.next-button");
@@ -60,7 +58,7 @@ if (pageURL.includes(4970020) && pageURL.includes('highholidays')) {
     }
 
     nextButton.addEventListener('click', function () {
-        setTimeout(scrollToPageTwo, 400);
+        setTimeout(scrollToPageTwo, 800);
     });
 
     amountsButtons.forEach(function (amountsButton) {

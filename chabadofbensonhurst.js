24a821d5b380d247899c30c28e2f97cfea6a254a
Mendel Gordon <mendelgordons@gmail.com>
2025-01-06 09:45:51 -0500

    Refactor script to prevent duplicate loading

--- chabadofbensonhurst.js
+++ chabadofbensonhurst.js
@@ -1,10 +1,9 @@
-const matcherIntoHtml = `
-<h3 class="fs-matchers-intro text-center js-viewport-in a-delay-100ms" data-effect="fadeIn" data-effect-offset="0">
-<span><strong>Every $ = 2</strong></span>
-<span>Double your impact thanks to our generous matchers!</span>
-</h3>`;
-
-const matcherInfoSelector = '.fs-matchers-intro';
-
-const matcherInfo = document.querySelector(matcherInfoSelector);
-if (matcherInfo) matcherInfo.outerHTML = matcherIntoHtml;
+// this script will be loaded twice, so we need to check if the element already exists
+// @ts-ignore
+if (typeof matcherIntroHtml === 'undefined') {
+    var matcherIntroHtml = `<h3 class="fs-matchers-intro text-center js-viewport-in a-delay-100ms" data-effect="fadeIn" data-effect-offset="0"><span><strong>Every $ = 2</strong></span><span>Double your impact, thanks to our generous matchers!</span></h3>`;
+    var matcherIntro = document.querySelector('.fs-matchers-intro');
+    if (matcherIntro) {
+        matcherIntro.outerHTML = matcherIntroHtml;
+    }
+}

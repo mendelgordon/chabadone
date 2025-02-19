6fc9d3a686eb8bcaac013d1e55c453c7342ebf9d
Mendel Gordon <mendelgordons@gmail.com>
2025-02-18 22:02:57 -0500

    Fix bottom shadow issue Fix IDE warning Move back to site link to fix top shadow issue Increase text size

--- friendscleveland/honor.js
+++ friendscleveland/honor.js
@@ -2,8 +2,14 @@ const elements = {
     donateButton: document.querySelector('.donate-button'),
     backButton: document.querySelector('.back-button a'),
     mainContent: document.querySelector('.main-content'),
-    sidebarForm: document.querySelector('.sidebar-form')
+    sidebarForm: document.querySelector('.sidebar-form'),
+    backToSite: document.querySelector('.cco_templateless_template'),
 };
+
+if (elements.backToSite) {
+    // move back to site link to the beginning of the main content
+    elements.mainContent?.insertBefore(elements.backToSite, elements.mainContent.firstChild);
+}
 
 if (elements.donateButton) {
     elements.donateButton.addEventListener('click', () => toggleView(true));
